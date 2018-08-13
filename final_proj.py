2657b9c935805c59251af252b0b2a0d74db6f622
wajeeh20-meet <40924490+wajeeh20-meet@users.noreply.github.com>
2018-08-13 06:04:24 -0400

    Add files via upload

---
+++ final_proj.py
@@ -0,0 +1,39 @@
+import turtle
+import time
+turtle.bgpic("bg.gif")
+lines = turtle.clone()
+lines.penup()
+lines.hideturtle()
+##turtle.hideturtle()
+lines.goto(0,300)
+lines.pendown()
+lines.color('white')
+lines.write("Clairdan Saving The Environment!" , align = 'center', font= ('impact', 30, "normal"))
+lines.penup()
+lines.goto(0,250)
+lines.pendown()
+lines.goto(400,250)
+lines.goto(400,-250)
+lines.goto(-400,-250)
+lines.goto(-400,250)
+lines.goto(0,250)
+lines.penup()
+SIZE_X=700
+SIZE_Y=400
+turtle.setup(1000, 1000)
+turtle.register_shape("clairdan.gif")
+turtle.register_shape("clairdan_back.gif")
+turtle.register_shape("enemy.gif")
+clairdan = turtle.clone()
+enemy = turtle.clone()
+enemy.shape("enemy.gif")
+clairdan.shape("clairdan.gif")
+
+enemy.shape("enemy.gif")
+clairdan.penup()
+enemy.penup()
+
+clairdan.goto(-200,140)
+enemy.goto(200,120)
+
+
